51b35ac7578800806944a5a3eb60190c594a6f94
Zijie Qiu <Qiu@Zijies-MacBook-Pro.local>
2019-01-21 10:55:27 -0600

    file submission

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -1,16 +1,45 @@
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
+#include "intro.h"
 
+#include <cmath>
 #include <string>
 
+using cs225::HSLAPixel;
+using cs225::PNG;
 
-void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+void rotate(std:: string inputFile, std:: string outputFile) {
+  PNG input;
+  input.readFromFile(inputFile);
+  PNG output(input.width(),input.height());
+  for (unsigned x = 0; x < input.width(); x++) {
+    for (unsigned y = 0; y < input.height(); y++) {
+      HSLAPixel & inputPixel = input.getPixel(x, y);
+      HSLAPixel & outputPixel = output.getPixel(input.width()-x-1,input.height()-y-1);
+      outputPixel=inputPixel;
+    }
+  }
+  output.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
-  // TODO: Part 3
-
+  for (unsigned x = 0; x < png.width(); x++) {
+    for (unsigned y = 0; y < png.height(); y++) {
+      HSLAPixel & pixel = png.getPixel(x,y);
+      pixel.h=180/cos((y-x)/360*3.141)*cos(60*x/width)+20;
+      pixel.a=1;
+      pixel.l=fmin(0.5+x/width,0.9);
+      double distance=x*x+y*y;
+      double diameter=width*width;
+      if((distance<diameter && distance> diameter/1.5)
+      || (distance<diameter/10 && distance> diameter/15)
+      || (distance<diameter/49 && distance> diameter/70 )){
+        pixel.s=1;
+      } else {
+        pixel.s=0.6;
+      }
+    }
+  }
   return png;
 }
